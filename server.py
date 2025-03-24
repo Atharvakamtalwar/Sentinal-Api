7e9f8edfddc0024a5765b78d31f8234aad6d36d0
Atharva Rajesh Kamtalwar <atharvakamtalwar@gmail.com>
2025-03-24 10:54:09 +0530

    Update server.py

--- server.py
+++ server.py
@@ -11,18 +11,13 @@ with open("final_model.pkl", "rb") as f:
     model = pickle.load(f)
 
 # Load the CSV data
-df = pd.read_csv("ModelTrainingCSV/sampled_data.csv")
+df = pd.read_csv("./sampled_data.csv")
 
 request_counter = 0
 
-origins = [
-    "http://localhost:5173",  # Vite/React Dev Server
-    "http://127.0.0.1:5173"   # Alternate local address
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # List of allowed origins
+    allow_origins=["*"],  # List of allowed origins
     allow_credentials=True,
     allow_methods=["*"],    # Allow all HTTP methods
     allow_headers=["*"],    # Allow all headers
